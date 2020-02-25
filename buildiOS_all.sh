0765fc8efed9aae1318727c418454057ede7d5da
Hendrik Holtmann <holtmann@mac.com>
2020-02-25 20:12:37 +0100

    Added exports an iOS scripts

---
+++ buildiOS_all.sh
@@ -0,0 +1,32 @@
+export CFLAGS="-fembed-bitcode -fapplication-extension"
+export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
+mkdir ~/Desktop/libsmb2
+export PREFIX="$HOME/Desktop/libsmb2"
+autoreconf -i
+make clean
+./iconfigure x86_64 --without-libkrb5 --disable-werror
+make
+make install
+make clean
+./iconfigure i386 --without-libkrb5 --disable-werror
+make
+make install
+make clean
+./iconfigure arm64 --without-libkrb5 --disable-werror
+make
+make install
+make clean
+./iconfigure armv7 --without-libkrb5 --disable-werror
+make
+make install
+make clean
+cd ~/Desktop/libsmb2/iphonesimulator-13.2/x86_64/lib
+install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.2.dylib
+cd ~/Desktop/libsmb2/iphonesimulator-13.2/i386/lib
+install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.2.dylib
+cd ~/Desktop/libsmb2/iphoneos-13.2/arm64/lib
+install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.2.dylib
+cd ~/Desktop/libsmb2/iphoneos-13.2/armv7/lib
+install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.2.dylib
+cd ~/Desktop/libsmb2/
+lipo -create iphonesimulator-13.2/x86_64/lib/libsmb2.2.dylib iphonesimulator-13.2/i386/lib/libsmb2.2.dylib iphoneos-13.2/arm64/lib/libsmb2.2.dylib iphoneos-13.2/armv7/lib/libsmb2.2.dylib -output libsmb2.2.dylib
