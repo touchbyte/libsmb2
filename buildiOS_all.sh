a5b97784fa29c5b5544ea8dcd14f22fc740833b1
Hendrik Holtmann <holtmann@mac.com>
2020-11-20 22:56:53 +0100

    Updated build scripts

--- buildiOS_all.sh
+++ buildiOS_all.sh
@@ -1,32 +1,57 @@
 export CFLAGS="-fembed-bitcode -fapplication-extension"
 export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
-mkdir ~/Desktop/libsmb2
-export PREFIX="$HOME/Desktop/libsmb2"
+mkdir ~/Desktop/libsmb2_new
+export PREFIX="$HOME/Desktop/libsmb2_new"
 autoreconf -i
+
 make clean
-./iconfigure x86_64 --without-libkrb5 --disable-werror
+export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
+CHOST=x86_64-apple-darwin SDK=iphonesimulator ./iconfigure x86_64 --without-libkrb5 --disable-werror
 make
 make install
-make clean
-./iconfigure i386 --without-libkrb5 --disable-werror
-make
-make install
-make clean
-./iconfigure arm64 --without-libkrb5 --disable-werror
-make
-make install
-make clean
-./iconfigure armv7 --without-libkrb5 --disable-werror
-make
-make install
-make clean
-cd ~/Desktop/libsmb2/iphonesimulator-13.2/x86_64/lib
+
+#make clean
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -miphonesimulator-version-min=13.0"
+#CHOST=arm-apple-darwin SDK=iphonesimulator TARGET=arm64-apple-ios13.0-simulator ./iconfigure arm64 --without-libkrb5 --disable-werror
+#make
+#make install
+
+#make clean
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
+#./iconfigure arm64 --without-libkrb5 --disable-werror
+#make
+#make install
+#exit
+
+#make clean
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
+#./iconfigure armv7 --without-libkrb5 --disable-werror
+#make
+#make install
+#make clean
+#exit
+
+#make clean
+#
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.0 -Xlinker 10.15"
+#CHOST=x86_64-apple-darwin SDK=macosx TARGET=x86_64-apple-ios13.0-macabi ./iconfigure x86_64 --without-libkrb5 --disable-werror
+#make
+#make install
+
+#make clean
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.0 -Xlinker 11.0"
+#CHOST=aarch64-apple-darwin22 SDK=macosx TARGET=arm64-apple-ios13.0-macabi ./iconfigure arm64 --without-libkrb5 --disable-werror
+#make
+#make install
+exit
+
+cd ~/Desktop/libsmb2/iphonesimulator-13.4/x86_64/lib
 install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.3.dylib
-cd ~/Desktop/libsmb2/iphonesimulator-13.2/i386/lib
+cd ~/Desktop/libsmb2/iphonesimulator-13.4/i386/lib
 install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.3.dylib
-cd ~/Desktop/libsmb2/iphoneos-13.2/arm64/lib
+cd ~/Desktop/libsmb2/iphoneos-13.4/arm64/lib
 install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.3.dylib
-cd ~/Desktop/libsmb2/iphoneos-13.2/armv7/lib
+cd ~/Desktop/libsmb2/iphoneos-13.4/armv7/lib
 install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.3.dylib
 cd ~/Desktop/libsmb2/
-lipo -create iphonesimulator-13.2/x86_64/lib/libsmb2.3.dylib iphonesimulator-13.2/i386/lib/libsmb2.3.dylib iphoneos-13.2/arm64/lib/libsmb2.3.dylib iphoneos-13.2/armv7/lib/libsmb2.3.dylib -output libsmb2.3.dylib
+#lipo -create iphonesimulator-13.4/x86_64/lib/libsmb2.3.dylib iphonesimulator-13.4/i386/lib/libsmb2.3.dylib iphoneos-13.4/arm64/lib/libsmb2.3.dylib iphoneos-13.4/armv7/lib/libsmb2.3.dylib -output libsmb2.3.dylib
