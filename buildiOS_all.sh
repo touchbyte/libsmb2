39eed1953e047db26539ddd8412f4642c4e4a65d
Hendrik Holtmann <holtmann@mac.com>
2020-11-30 03:40:06 +0100

    Update buildiOS_all.sh

--- buildiOS_all.sh
+++ buildiOS_all.sh
@@ -4,11 +4,11 @@ mkdir ~/Desktop/libsmb2_new
 export PREFIX="$HOME/Desktop/libsmb2_new"
 autoreconf -i
 
-make clean
-export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
-CHOST=x86_64-apple-darwin SDK=iphonesimulator ./iconfigure x86_64 --without-libkrb5 --disable-werror
-make
-make install
+#make clean
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension"
+#CHOST=x86_64-apple-darwin SDK=iphonesimulator ./iconfigure x86_64 --without-libkrb5 --disable-werror
+#make
+#make install
 
 #make clean
 #export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -miphonesimulator-version-min=13.0"
@@ -31,19 +31,21 @@ make install
 #make clean
 #exit
 
-#make clean
-#
-#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.0 -Xlinker 10.15"
-#CHOST=x86_64-apple-darwin SDK=macosx TARGET=x86_64-apple-ios13.0-macabi ./iconfigure x86_64 --without-libkrb5 --disable-werror
-#make
-#make install
+make clean
+export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.1 -Xlinker 14.0"
+export CHOST=x86_64-apple-darwin SDK=macosx TARGET=x86_64-apple-ios13.1-macabi 
+./iconfigure x86_64 --without-libkrb5 --disable-werror
+make
+make install
+exit
 
 #make clean
-#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.0 -Xlinker 11.0"
-#CHOST=aarch64-apple-darwin22 SDK=macosx TARGET=arm64-apple-ios13.0-macabi ./iconfigure arm64 --without-libkrb5 --disable-werror
+#export LDFLAGS="-Wc,-fembed-bitcode,-fapplication-extension -Xlinker -platform_version -Xlinker mac-catalyst -Xlinker 13.1 -Xlinker 14.0 -target arm64-apple-ios13.1-macabi"
+#export CHOST=aarch64-apple-darwin22 SDK=macosx TARGET=arm64-apple-ios13.1-macabi 
+#./iconfigure arm64 --without-libkrb5 --disable-werror
 #make
 #make install
-exit
+#exit
 
 cd ~/Desktop/libsmb2/iphonesimulator-13.4/x86_64/lib
 install_name_tool -id @rpath/libsmb2.framework/libsmb2 libsmb2.3.dylib
